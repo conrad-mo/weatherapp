7c6dce34b5f6a2d6632231067ec282f4464435e0
Conrad Mo <51024076+conrad-mo@users.noreply.github.com>
2023-01-27 00:22:21 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,45 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MaterialApp(
+    title: 'Weather App',
+    home: Scaffold(
+      body: WeatherBBar(),
+    ),
+  ));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class WeatherScreen extends StatefulWidget {
+  const WeatherScreen({super.key});
 
   @override
+  State<WeatherScreen> createState() => _WeatherScreenState();
+}
+
+class _WeatherScreenState extends State<WeatherScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: WeatherBBar(),
-        ),
+    return const Scaffold(
+      body: Center(
+        child: Text('Pog'),
+      ),
+    );
+  }
+}
+
+class UvScreen extends StatefulWidget {
+  const UvScreen({super.key});
+
+  @override
+  State<UvScreen> createState() => _UvScreenState();
+}
+
+class _UvScreenState extends State<UvScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('POG'),
       ),
     );
   }
@@ -27,21 +53,37 @@ class WeatherBBar extends StatefulWidget {
 }
 
 class _WeatherBBarState extends State<WeatherBBar> {
+  int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: _onTabItemTapped,
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.sunny),
-            label: 'UV Index',
+            label: 'Home',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.explore),
-            label: 'Weather',
+            label: 'Location',
           ),
         ],
       ),
+      body: IndexedStack(
+        index: _currentIndex,
+        children: const [
+          WeatherScreen(),
+          UvScreen(),
+        ],
+      ),
     );
+  }
+
+  void _onTabItemTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
   }
 }
