e9e6cd1cfd0df434b2895701dc60415fa698e840
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2023-06-10 18:58:55 -0400

    Wrapped with padding for improved UI

--- lib/components/weathermain.dart
+++ lib/components/weathermain.dart
@@ -11,42 +11,44 @@ class _WeatherMainContainerState extends State<WeatherMainContainer> {
   @override
   Widget build(BuildContext context) {
     return Align(
-      alignment: Alignment.topCenter,
-      child: Row(
-        children: <Widget>[
-          const Icon(
-            Icons.sunny,
-            size: 35.0,
-            semanticLabel: 'Weather icon',
-          ),
-          const Positioned(
-            bottom: 1,
-            child: Text('Sunny and warm'),
-          ),
-          const Spacer(),
-          Align(
-            alignment: Alignment.topRight,
-            child: RichText(
-              text: TextSpan(
-                style: DefaultTextStyle.of(context).style,
-                children: const <TextSpan>[
-                  TextSpan(
-                      text: '26',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 75.0)),
-                  TextSpan(
-                    text: '\u1d52',
-                    style: TextStyle(
-                      fontSize: 75.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
+        alignment: Alignment.topCenter,
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 300),
+          child: Row(
+            children: <Widget>[
+              const Icon(
+                Icons.sunny,
+                size: 35.0,
+                semanticLabel: 'Weather icon',
               ),
-            ),
+              const Positioned(
+                bottom: 1,
+                child: Text('Sunny and warm'),
+              ),
+              const Spacer(),
+              Align(
+                alignment: Alignment.topRight,
+                child: RichText(
+                  text: TextSpan(
+                    style: DefaultTextStyle.of(context).style,
+                    children: const <TextSpan>[
+                      TextSpan(
+                          text: '26',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 75.0)),
+                      TextSpan(
+                        text: '\u1d52',
+                        style: TextStyle(
+                          fontSize: 75.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
-    );
+        ));
   }
 }
